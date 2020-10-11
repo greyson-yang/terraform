bfdddaf1aefa90bf89388122b8a77ad241be29c7
greyson-yang <greyson.yang.it@gmail.com>
2020-10-11 13:30:27 +1100

    basic ec2 with sg

--- main.tf
+++ main.tf
@@ -2,7 +2,38 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
+variable "server_port" {
+  description    = "The Port server will use for Http requests"
+  type           = number
+  default	 = 8080
+}
+
 resource "aws_instance" "example" {
   ami		= "ami-0f158b0f26f18e619"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, Greyson" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port	= var.server_port
+    to_port	= var.server_port
+    protocol	= "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_ip" {
+  value		= aws_instance.example.public_ip
+  description   = "The public IP address of instance"
 }
